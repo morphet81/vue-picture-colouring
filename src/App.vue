1c21109335472ce367266ccf014fb6b77ab3e433
alexandre <aobellianne@phoceis.asia>
2018-07-13 18:51:26 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vic-image :color="colors[current]" :erase="erase" :toolWidth="26" :width="640*.7" :height="480*.7" class="vic-image" :src="require('./assets/watch-sketch-example.png')"></vic-image>
+    <div class="colors">
+      <div class="color-container" v-for="(color, i) in colors" :key="i">
+        <div class="color" :style="colorStyle(i)" @click="onColorClick(i)"></div>
+      </div>
+      <div class="color-container">
+        <div class="color" :style="eraserStyle" @click="erase = !erase">Eraser</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './components/index.js'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      erase: false,
+      current: 0,
+      colors: [
+        '#dd3b3b',
+        '#3bdd58',
+        '#3b76dd',
+        '#c73bdd'
+      ]
+    }
+  },
+  computed: {
+    eraserStyle () {
+      return {
+        'border': this.erase ? 'solid 4px black' : ''
+      }
+    }
+  },
+  methods: {
+    colorStyle (index) {
+      return {
+        'background-color': this.colors[index],
+        'border': !this.erase && this.current == index ? 'solid 4px black' : ''
+      }
+    },
+    onColorClick (index) {
+      this.current = index
+      this.erase = false
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  position: fixed;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.vic-image {
+  width: 100vw;
+  height: 75vh;
+}
+
+.colors {
+  display: flex;
+  flex-direction: row;
+}
+
+.color-container {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+
+.color {
+  width: 8vw;
+  height: 12vh;
 }
 </style>
